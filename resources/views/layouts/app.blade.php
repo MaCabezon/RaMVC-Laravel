616461ae67a3a5731b3df5627889615e086f0677
Miguel Cabezon <miguel.cabezon@alumnos.uneatlantico.es>
2018-04-16 14:22:16 +0200

    App

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,18 +18,8 @@
     <script src="http://code.highcharts.com/highcharts.js"></script>
     <script src="{{ asset('js//bootstrap.min.js') }}"></script>
     <script>
-      
+
       $(document).ready(function () {
-
-<<<<<<< HEAD
-                      (function ($) {
-=======
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="{{ asset('js//bootstrap.min.js') }}"></script>
-  <script>
-
-    $(document).ready(function () {
->>>>>>> miguel
 
                           $('#filtrar').keyup(function () {
 
