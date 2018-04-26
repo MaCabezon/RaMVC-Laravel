6217c97101fc021545a612030806524186ebefd7
abraham.fernandez <abraham.fernandez@alumnos.uneatlantico.es>
2018-04-26 12:31:05 +0200

    Update 2018_03_27_110950_create_resumen_alumnos_view.php

--- database/migrations/2018_03_27_110950_create_resumen_alumnos_view.php
+++ database/migrations/2018_03_27_110950_create_resumen_alumnos_view.php
@@ -14,15 +14,14 @@ class CreateResumenAlumnosView extends Migration
     public function up()
     {
 
-         DB::statement( "CREATE VIEW  resumenalumnos as select ra.idAlumno AS Alumno,ev.nombre AS Evento,ev.grupo AS Grupo,
-						 (CASE WHEN ra.horas = -1.00 && ra.validado = '0' THEN 'activado/pendiente'
-						 WHEN ra.horas > -1.00 && ra.validado = '0' THEN 'desactivado/pendiente' 
+         DB::statement( "CREATE VIEW  resumenalumnos as select ra.idAlumno AS Alumno,ev.nombre AS Evento,ev.grupo AS Grupo, 
+						 (CASE WHEN ra.horas = -1.00 && ra.validado = '0' THEN 'P.Activado'
+						 WHEN ra.horas > -1.00 && ra.validado = '0' THEN 'P.desactivado'
 						 WHEN ra.horas = -1.00 && ra.validado = '1' THEN 'activado' 
 						 WHEN ra.horas > -1.00 && ra.validado = '1' THEN 'desactivado' END) AS Estado, 
-						 ra.fechaEvento AS fechaEvento,ra.validado AS Validado
+						 ra.fechaEvento AS fechaEvento,ra.validado AS Validado 
 						 from (rapframework.resumen_alumnos ra join rapframework.eventos ev 
-						 on((ra.idEvento = ev.id))) where ra.id in (select max(r.id)
-						 from rapframework.resumen_alumnos r where (r.idAlumno = ra.idAlumno)) 
+						 on((ra.idEvento = ev.id))) where ra.id in (select max(r.id) from rapframework.resumen_alumnos r where (r.idAlumno = ra.idAlumno)) 
 						 group by ra.idAlumno,ra.idEvento,ra.fechaEvento");
 
     }
