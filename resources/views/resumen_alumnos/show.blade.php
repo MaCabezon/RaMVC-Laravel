ff8dcc4d98dd3675b5704b159f292267c6ac15f9
Miguel Cabezon <miguel.cabezon@alumnos.uneatlantico.es>
2018-04-26 15:58:53 +0200

    Update show.blade.php

--- resources/views/resumen_alumnos/show.blade.php
+++ resources/views/resumen_alumnos/show.blade.php
@@ -1,17 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-    <section class="content-header">
-        <h1>
-            Resumen Alumnos
-        </h1>
-    </section>
+    
     <div class="content">
         <div class="box box-primary">
             <div class="box-body">
                 <div class="row" style="padding-left: 20px">
                     @include('resumen_alumnos.show_fields')
-                    <a href="{!! route('resumenAlumnos.index') !!}" class="btn btn-default">Atras</a>
+
                 </div>
             </div>
         </div>
