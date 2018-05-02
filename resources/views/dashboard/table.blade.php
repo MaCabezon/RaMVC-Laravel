e4aff4e1c0f7e041c12384668499050a9de92520
abraham.fernandez <abraham.fernandez@alumnos.uneatlantico.es>
2018-05-02 16:03:56 +0200

    Update table.blade.php

--- resources/views/dashboard/table.blade.php
+++ resources/views/dashboard/table.blade.php
@@ -1,95 +1,94 @@
-  <div id ="encabezado" class="col-lg-12" class="label label-default" >
-   <h3>Uneatlantico Asistencias</h3>
-   <hr/>
-  </div>
+<div id ="encabezado" class="col-lg-12" class="label label-default" >
+         <h3>Uneatlantico Asistencias</h3>
+         <hr/>
+</div>
 
-  <div class="col-lg-12" id="contenedor_dashboard" >
-    <div class="col-md-12">
+   <!-- <div class="container" >-->
+        <!--<div class="row">-->
 
-      <!-- Variable que guarda el evento -->
-      @php
-      $eventoAct ="";
-      $flagTable = false;
-      @endphp
+        <!-- Variable que guarda el evento -->
+        @php
+          $eventoAct ="";
+          $flagTable = false;
+        @endphp
 
-      <?php $key=0; ?>
-      <?php $break=false; ?>
-      <?php $keyAux=0; ?>
-      <?php $i=0; ?>
-      <?php $table=0; ?>
+        <?php $key=0; ?>
+        <?php $break=false; ?>
+        <?php $keyAux=0; ?>
+        <?php $i=0; ?>
+        <?php $table=0; ?>
 
-      @for ($key = 0; $key < count($vista); $key++)
+        @for ($key = 0; $key < count($vista); $key++)
 
-      @if($vista[$key]->Evento!=$eventoAct)
-        @if($flagTable)
-           </tbody>
+            @if($vista[$key]->Evento!=$eventoAct)
+            @if($flagTable)
+                    </tbody>
 
 
-
-          @if($table%2==0)
+            </table>
+            @if($table%2==0)
             </div>
-       @endif
-      @endif
+            @endif
+            @endif
 
-     @php
-      $flagTable = true;
-     @endphp
+            @php
+              $flagTable = true;
+            @endphp
 
-  <?php $table++; ?>
-  @if($table%2!=0)
+            <?php $table++; ?>
+            @if($table%2!=0)
+              <div  class="col-lg-12" id="contenedor_materia" >
+            @endif
 
-  <div class="col-md-6 " id="contenedor_materia" >
-    @endif
+            <!-- Si falla poner a 45% -->
+              <table class="col-lg-6 offset-lg-2" id="tabla_asistencia" >
 
-     <!-- Si falla poner a 45% -->
-    <table class="table" id="tabla_asistencia" style=" float: left;">
+                  <thead class="thead-dark">
+                      <tr>
+                         <th colspan="5" scope="col">{!! $vista[$key]->Evento !!}</th>
+                       </tr>
+                  </thead>
+                  <tbody class="buscar">
+                @php
+                $eventoAct = $vista[$key]->Evento
+                @endphp
+              @endif
 
-     <thead class="thead-dark">
-        <tr>
-          <th colspan="5" scope="col">{!! $vista[$key]->Evento !!}</th>
-        </tr>
-     </thead>
-    <tbody class="buscar">
-      @php
-      $eventoAct = $vista[$key]->Evento
-      @endphp
-      @endif
+                   <tr>
 
-      <tr>
+                     @for($i=0; $i < 5; $i++)
+                      @if(isset($vista[$key+$i]) && $vista[$key+$i]->Evento==$eventoAct)
+                     <td id="{{$vista[$key+$i]->Alumno}}{{$vista[$key+$i]->Evento}}" style="float: left; border:inset 0pt;">
+                       @if($vista[$key+$i]->Estado=='activado')
+                        <img src="{{ asset('css/images/IconoV.png') }}" height="30px" width="30px"/>
+                       @elseif($vista[$key+$i]->Estado=='desactivado')
+                        <img src="{{ asset('css/images/IconoR.png') }}" height="30px" width="30px"/>
+                       @elseif (substr( $vista[$key+$i]->Estado, 0, 1 ) === "P")
+                        <img src="{{ asset('css/images/IconoA.png') }}" height="30px" width="30px"/>
+                       @endif
+                       <div style="float: right;">
+                       {!!$vista[$key+$i]->Alumno !!}
+                       </br>
+                       {!!$vista[$key+$i]->Horas !!}
+                       </div>
+                        </br>
+                        {!!$vista[$key+$i]->Estado !!}
+                    </td>
 
-         @for($i=0; $i < 5; $i++)
-         @if(isset($vista[$key+$i]) && $vista[$key+$i]->Evento==$eventoAct)
-        <td class= "col-lg-2" id="{{$vista[$key+$i]->Alumno}}{{$vista[$key+$i]->Evento}}" style="float: left; border: inset 0pt">
-          <div id="nombre"> {!!$vista[$key+$i]->Alumno !!} </div>
-          @if($vista[$key+$i]->Estado=='activado')
-          <img src="{{ asset('css/images/IconoV.png') }}" height="30%" width="30%"/>
-          @elseif($vista[$key+$i]->Estado=='desactivado')
-          <img src="{{ asset('css/images/IconoR.png') }}" height="30%" width="30%"/>
-          @elseif (substr( $vista[$key+$i]->Estado, 0, 1 ) === "P")
-          <img src="{{ asset('css/images/IconoA.png') }}" height="30%" width="30%"/>
-          @endif
+                    @else
+                      <?php break; ?>
+                    @endif
 
-          <div style="float: right;">
-            {!!$vista[$key+$i]->Estado !!}
-            </br>
-            {!!$vista[$key+$i]->Horas !!}
-          </div>
-        </td>
-
-        @else
-        <?php break; ?>
-        @endif
-
-        @endfor
-        <?php $key = $key + $i-1; ?>
-      </tr>
-
-
-     @endfor
-    </tbody>
+                    @endfor
+                    <?php $key = $key + $i-1; ?>
+                  </tr>
 
 
-  </table>
-  </div>
+    @endfor
+  </tbody>
 
-  </div>
+
+</table>
+<!--</div>-->
+
+    <!-- </div>-->
