2c45bfbe7507031476dff5c76ed1ae98b074eec5
carmen.godoy <carmen.godoy@alumnos.uneatlantico.es>
2018-05-16 08:38:54 +0200

    prueba imagen home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,16 @@
-@extends('layouts.app')
 
-@section('content')
+<div id="background_web" style="background-color: red;width: 900px; height: 1000px;">
+	@extends('layouts.app')
 
-@if ($message = Session::get('message'))
-<div  class="alert alert-danger" role="alert">
-	<button type="button" class="close" data-dismiss="alert">×</button>	
-	<strong>{{ $message }}</strong>
+	@section('content')
+
+	@if ($message = Session::get('message'))
+	<div  class="alert alert-danger" role="alert">
+		<button type="button" class="close" data-dismiss="alert">×</button>	
+		<strong>{{ $message }}</strong>
+	</div>
+	@endif
+
+	@endsection
+
 </div>
-@endif
-
-@endsection
