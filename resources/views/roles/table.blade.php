c8ccead8bb07c2a7a57140bcf35670aebea4da17
Abraham Fernandez <abraham.fernandez@alumnos.uneatlantico.es>
2018-05-17 09:16:05 +0200

    ajuste role view

--- resources/views/roles/table.blade.php
+++ resources/views/roles/table.blade.php
@@ -11,7 +11,7 @@
             <tr>
             <th>Nombre</th> 
             <th colspan="3">Acciones
-            @can('roles-create')
+            @can('role-create')
                     <a  class="glyphicon  plus btn-sm glyphicon-plus"  href="{!! route('roles.create') !!}"></a>
             @endcan
          </th>
@@ -25,13 +25,13 @@
                 <td>
                     {!! Form::open(['route' => ['roles.destroy', $role->id], 'method' => 'delete']) !!}
                     <div class='btn-group'>
-                    @can('roles-show')
+                    @can('role-show')
                         <a href="{!! route('roles.show', [$role->id]) !!}" class='btn btn-success btn-sm'><i class="glyphicon glyphicon-eye-open"></i></a>
                      @endcan
-                     @can('roles-edit')
+                     @can('role-edit')
                         <a href="{!! route('roles.edit', [$role->id]) !!}" class='btn btn-info btn-sm'><i class="glyphicon glyphicon-edit"></i></a>
                      @endcan
-                     @can('roles-delete')
+                     @can('role-delete')
                         {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-sm', 'onclick' => "return confirm('Are you sure?')"]) !!}
                     @endcan
                     </div>
