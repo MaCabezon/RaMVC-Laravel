be3e802934f878a0ed2f61a7468c320e29b1f751
Abraham Fernandez <abraham.fernandez@alumnos.uneatlantico.es>
2018-05-18 11:11:31 +0200

    reajustes

--- app/Http/Controllers/ResumenAlumnosController.php
+++ app/Http/Controllers/ResumenAlumnosController.php
@@ -76,7 +76,8 @@ class ResumenAlumnosController extends AppBaseController
             $resumenAlumno->fechaEvento=$resultado['fechaEvento'];
             $resumenAlumno->horas=$resultado['horas'];
             $resumenAlumno->validado=$resultado['validado'];            
-            $resumenAlumno->jusrificante=$resultado['justificante'];
+            $resumenAlumno->justificante=$resultado['justificante'];
+            $resumenAlumno->save();
     }
 
     /**
