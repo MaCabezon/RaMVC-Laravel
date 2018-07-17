41a71a1dca8f5b457c75696d1ebbd5a13f58248f
carmen.godoy <carmen.godoy@alumnos.uneatlantico.es>
2018-07-17 11:12:45 +0200

    favicon,estilos filtro

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,20 +8,24 @@
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
+
   <title>{{ config('app.name', 'Asistencias Uneatlantico') }}</title>
 
-
-    <!-- Styles -->
-    <!--<link href="{{ asset('css/app.css') }}" rel="stylesheet">-->
-    <!--Link del Datepicker-->
-    <link href="/css/bootstrap-datetimepicker.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
-    <link href="{{ asset('css/webService.css') }}" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-
-
+  <!-- Styles -->
+  <!--<link href="{{ asset('css/app.css') }}" rel="stylesheet">-->
+  <!--Link del Datepicker-->
+  <link href="/css/bootstrap-datetimepicker.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+  <link href="{{ asset('css/webService.css') }}" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <link rel="shortcut icon" href="../public/css/images/qrcode.ico">
   <script src="https://code.highcharts.com/highcharts.js"></script>
   <script src=" https://code.highcharts.com/modules/exporting.js"></script>
+
+
+
+
+
   <script src="{{ asset('js/bootstrap.min.js') }}"></script>
   <script>
 
@@ -36,7 +40,7 @@
         $('.buscar tr').filter(function () {
           return rex.test($(this).text());
         }).show();
- 
+
       });
 
     }(jQuery));
@@ -47,83 +51,108 @@
 </head>
 <body>
 
-  <nav class="navbar navbar-default navbar-expand-lg ">
-       <div class="Contenedor_nav col-lg-12"> 
+
+  <nav class="navbar navbar-default navbar-expand-lg">
+    <div class="Contenedor_nav col-lg-12">
       <!-- Brand and toggle get grouped for better mobile display -->
-      <div class="col-lg-1"> 
-        <a id="logo" class="navbar-brand " href="{{ action('HomeController@index') }}" style="margin-bottom: 10px; margin-top: 10px;margin-left: 1%;"></a>
+      <div class="col-lg-1" >
+         <a id="logo" class="navbar-brand col-lg-1" href="{{ action('HomeController@index') }}" style="margin-bottom: 10px; margin-top: 10px;margin-left: 1%"></a>
+      </div>
+      
+      <div class="col-lg-6" style="padding-left: 0%;padding-right: 0%;margin-left: 0%; margin-right: 0%;margin-top: 20px;float:left;"> 
+       @auth
+          <form class="navbar-form navbar-right" style="border:none;">
+            <input id="filtrar"  type="text" class="form-control" placeholder="Introduzca dato a buscar...">
+          </form>
+        @endauth  
       </div>
 
+      <div>
+        <button type="button" class="navbar-toggle collapsed sidenav" data-toggle="collapse" data-target="#navdos" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation" style= "margin-top:5%;">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+      </div>
 
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="{{ action('HighchartController@highchart') }}"><span class="glyphicon glyphicon-stats"></span> Estadísticas</a></li>
-        <li><a href="{{ action('DashboardController@index') }}"><span class="glyphicon glyphicon-dashboard"></span> Dashboard</a></li>
-        @auth
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Administracion <span class="caret"></span></a>
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse col-lg-1" id="navdos" style="padding-left: 0px;margin-top: 20px; float=right">
+        
+        <ul class="nav navbar-nav navbar-right">
+        
+
+          <li>
+            <a href="{{ action('HighchartController@highchart') }}"><span class="glyphicon glyphicon-stats"></span> Estadísticas</a>
+          </li>
+
+          <li>
+            <a href="{{ action('DashboardController@index') }}"><span class="glyphicon glyphicon-dashboard"></span> Dashboard</a>
+          </li>
+          @auth
+          <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Administracion <span class="caret"></span></a>
+            <ul class="dropdown-menu" >
+              @can('eventos-list')
+              <li><a href="{{ action('EventosController@index') }}">Eventos</a></li>
+              @endcan
+              @can('resumenAlumnos-list')
+              <li><a href="{{ action('ResumenAlumnosController@index') }}">Resumen Alumnos</a></li>
+              @endcan
+              @can('resumenEventos-list')
+              <li><a href="{{ action('ResumenEventosController@index') }}">Resumen Eventos</a></li>
+              @endcan
+              @can('transacciones-list')
+              <li><a href="{{ action('TransaccionesController@index') }}">Transacciones</a></li>
+              @endcan
+            </ul>
+          </li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Generar Reporte <span class="caret"></span></a>
+            <ul class="dropdown-menu" >
+             <li> <a href="reporteTable">Visualizar reporte</a></li>
+             <li> <a href="{{ action('ResumenAlumnosController@excel') }}">Descargar reporte</a></li>
+           </ul>
+          </li>
+         @endauth
+         @guest
+         <li><a class="nav-link" href="{{ route('login') }}"><span class="glyphicon glyphicon-log-in"></span> Log in
+         </a></li>
+         @endguest
+         @auth
+         <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+            @if($user = \Auth::user())
+            {{ stristr($user->email, '@', true)  }} <span class="caret"></span>
+            @endif
+          </a>
           <ul class="dropdown-menu" >
-          @can('eventos-list')
-            <li><a href="{{ action('EventosController@index') }}">Eventos</a></li>
-          @endcan
-          @can('resumenAlumnos-list')
-            <li><a href="{{ action('ResumenAlumnosController@index') }}">Resumen Alumnos</a></li>
-          @endcan
-          @can('resumenEventos-list')
-            <li><a href="{{ action('ResumenEventosController@index') }}">Resumen Eventos</a></li>
-          @endcan
-          @can('transacciones-list')
-            <li><a href="{{ action('TransaccionesController@index') }}">Transacciones</a></li>
-          @endcan
-          </ul>
-        </li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Generar Reporte <span class="caret"></span></a>
-          <ul class="dropdown-menu" >
-           <li> <a href="reporteTable">Visualizar reporte</a></li>
-           <li> <a href="{{ action('ResumenAlumnosController@excel') }}">Descargar reporte</a></li>
+            @can('user-list')
+            <li><a href="{{ action('UserController@index') }}">Usuario</a></li>
+            @endcan
+            @can('role-list')
+            <li><a href="{{ action('RoleController@index') }}">Roles</a></li>
+            @endcan
+            @can('permissions-list')
+            <li><a href="{{ action('PermissionController@index') }}">Permisos</a></li>
+            @endcan
+            <li>
+              <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                {{ __('Logout') }}
+              </a>
+
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+              </form>
+            </li>
+
           </ul>
         </li>
         @endauth
-        @guest
-          <li><a class="nav-link" href="{{ route('login') }}"><span class="glyphicon glyphicon-log-in"></span> Log in
-          </a></li>
-        @endguest
-        @auth
-        <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-          @if($user = \Auth::user())
-                    {{ stristr($user->email, '@', true)  }} <span class="caret"></span>
-          @endif
-        </a>
-            <ul class="dropdown-menu" >
-              @can('user-list')
-                <li><a href="{{ action('UserController@index') }}">Usuario</a></li>
-              @endcan
-              @can('role-list')
-                <li><a href="{{ action('RoleController@index') }}">Roles</a></li>
-              @endcan
-              @can('permissions-list')
-                <li><a href="{{ action('PermissionController@index') }}">Permisos</a></li>
-              @endcan
-                <li>
-                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                            {{ __('Logout') }}
-                        </a>
-
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            @csrf
-                        </form>
-                </li>
-
-            </ul>
- </li>
-      @endauth
-     </ul>
+      </ul>
     </div><!-- /.navbar-collapse -->
-
   </div><!-- /.container-fluid -->
 </nav>
-
+<div>
   <main class="py-4">
     @yield('content')
   </main>
--- resources/views/resumen_eventos/index.blade.php
+++ resources/views/resumen_eventos/index.blade.php
@@ -2,76 +2,89 @@
 
 @section('content')
 
-    <div class="content">
-        <div class="clearfix"></div>
+<div class="content">
+  <div class="clearfix"></div>
 
-        @include('flash::message')
+  @include('flash::message')
 
-        <div class="clearfix">
-          <div id ="encabezado" class="col-lg-12" class="label label-default" >
-                   <h3>Resumen de Eventos</h3>
-                   <hr/>
-          </div>
-          <div class="container">
-	           <div class="row">
-		             <div class="col-md-12">
-		                 <form class="form-inline form-filtro">
-                       <div class="form-group">
-                         <h6>Fecha inicial</h6>
-                         <input type="date" class="form-control" id="fechaInicial" name="fechaInicial" max="<?php $hoy=date('Y-m-d'); echo $hoy; ?>">
-                       </div>
-                       <div class="form-group">
-                         <h6>Fecha final</h6>
-                         <input type="date" class="form-control" id="fechaFinal" name="fechaFinal" max="<?php $hoy=date('Y-m-d'); echo $hoy; ?>">
-                       </div>
+  <div class="clearfix">
+      <div id ="encabezado" class="col-lg-12" class="label label-default" >
+        <h3>Resumen de Eventos</h3>
+        <hr/>
+      </div>
 
-                         @php
-                          $arrayEventos = [];
-                         @endphp
-
-                        @foreach ($resumenEventosCompleto as $resumenEvento)
-                          @php
-                            $arrayEventos[] = $resumenEvento->nombre;
-                            asort($arrayEventos);
-                          @endphp
-                        @endforeach
-
-
-                        {!! Form::open(['route' => ['resumenAlumnos.index'], 'method' => 'POST']) !!}
-
-                        <div class="form-group">
-                          <h6>Materia</h6>
-                          <select name="eventos" class="form-control">
-                            <option value="" disabled selected>-- Seleccione una materia --</option>
-                            @foreach (array_unique($arrayEventos) as $key => $value)
-                              <option value="{{ $value }}">{{ $value }}</option>
-                            @endforeach
-                          </select>
-                        </div>
-                        <div class="form-group">
-                        </br>
-                        <!-- 'btn btn-primary btn-sm' -->
-
-                          {!! Form::button('Filtrar',['type' => 'submit', 'class' => 'btn btn-primary', 'method' => 'delete']) !!}
-                          {!! Form::button('Limpiar',['type' => 'reset', 'class' => 'btn btn-default']) !!}
-                        </div>
-                        {!! Form::close() !!}
-
-                     </form>
-		                 </div>
-	                  </div>
+      <div class="container">
+        <div>
+        <div>
+            <div class="col-md-8" style="padding-left: 20%; padding-right: 0%">
+               <form class="form-inline form-filtro">
+                <div class=col-md-12 style="padding-left: 0%">
+                  <div class="form-group col-md-6" style="padding-left: 0%">
+                     <h6>Fecha inicial</h6>
+                     <input type="date" class="form-control" id="fechaInicial" name="fechaInicial" max="<?php $hoy=date('Y-m-d'); echo $hoy; ?>">
                   </div>
-
-                </div>
-        <div class="box box-primary">
-            <div class="box-body">
-              @if($hasFilter==true)
-                  @include('resumen_eventos.table')
-              @endif
+                  <div class="form-group col-md-6" style="padding-left: 6%">
+                     <h6>Fecha final</h6>
+                     <input type="date" class="form-control" id="fechaFinal" name="fechaFinal" max="<?php $hoy=date('Y-m-d'); echo $hoy; ?>">
+                  </div>
             </div>
-        </div>
-        <div class="text-center">
+       @php
+       $arrayEventos = [];
+       @endphp
 
-        </div>
+       @foreach ($resumenEventosCompleto as $resumenEvento)
+       @php
+       $arrayEventos[] = $resumenEvento->nombre;
+       asort($arrayEventos);
+       @endphp
+       @endforeach
+
+
+       {!! Form::open(['route' => ['resumenAlumnos.index'], 'method' => 'POST']) !!}
+
+
+           
+           <div class="form-group col-md-12">
+             <h6>Materia</h6>
+             <select name="eventos" class="form-control">
+             <option value="" disabled selected>-- Seleccione una materia --</option>
+             @foreach (array_unique($arrayEventos) as $key => $value)
+             <option value="{{ $value }}">{{ $value }}</option>
+             @endforeach
+             </select>
+           </div>
+      </div>
+
+
+      <div class="form-group col-md-2" style="padding-left: 2%;padding-bottom: 0%;padding-top: 3.5%;float: left;">
+      </br>
+      <!-- 'btn btn-primary btn-sm' -->
+
+      {!! Form::button('Filtrar',['type' => 'submit', 'class' => 'btn btn-primary', 'method' => 'delete']) !!}
+
+      {!! Form::button('Limpiar',['type' => 'reset', 'class' => 'btn btn-default']) !!}
     </div>
+
+
+    {!! Form::close() !!}
+    
+      </div>
+    </div>
+    </form>
+</div>
+</div>
+</div>
+
+
+<div class="box box-primary">
+  <div class="box-body">
+    @if($hasFilter==true)
+    @include('resumen_eventos.table')
+    @endif
+  </div>
+</div>
+<div class="text-center">
+
+</div>
+</div>
 @endsection
