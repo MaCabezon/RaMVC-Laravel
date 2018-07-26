14deaa8674e43eef9a31221def1d77aa9720d5c7
abraham.fernandez <abraham.fernandez@alumnos.uneatlantico.es>
2018-07-26 13:22:27 +0200

    ajuste filtro transacciones rol member

--- app/Http/Controllers/TransaccionesController.php
+++ app/Http/Controllers/TransaccionesController.php
@@ -91,7 +91,7 @@ class TransaccionesController extends AppBaseController
       }
 
       // Seleccion de datos con FILTRO
-      if (\Auth::user()->hasRole('admin'))
+      if (\Auth::user()->hasRole('admin')||\Auth::user()->hasRole('member'))
       {
         $query = DB::table('transaccionesView');
 
@@ -115,7 +115,35 @@ class TransaccionesController extends AppBaseController
 
         $transacciones = $query->orderBy('fechaEvento', 'DESC')
             ->get();
-      }
+      }else if (\Auth::user()->hasRole('member'))
+      {	
+			$query = DB::table('transaccionesView')
+				     ->where('nombre','Becas I')
+					 ->orWhere('nombre', 'Becas II')
+					 ->orWhere('nombre', 'Intervencion Agil I')
+					 ->orWhere('nombre','Intervencion Agil II');
+					 
+			if (!is_null($f1) && is_null($f2)) {
+			  $f2 = date('Y-m-d');
+			  $query->whereBetween('fechaEvento', [Carbon::parse($f1)->startOfDay(), Carbon::parse($f2)->endOfDay()]);
+			}
+			if (is_null($f1) && !is_null($f2)) {
+			  $query->where('fechaEvento', '<=', Carbon::parse($f2)->endOfDay());
+			}
+			if (!is_null($f1) && !is_null($f2)) {
+			  $query->whereBetween('fechaEvento', [Carbon::parse($f1)->startOfDay(), Carbon::parse($f2)->endOfDay()]);
+			}
+
+			if ($evento != null) {
+			  $query->where('nombre',$evento);
+			}
+			if ($alumno != null) {
+			  $query->where('idPersona',$alumno);
+			}
+
+			$transacciones = $query->orderBy('fechaEvento', 'DESC')
+				->get();		 
+	   }
       
 
 
