d39bdb558c7f13460872e07f316ba4810663b1d6
abraham.fernandez <abraham.fernandez@alumnos.uneatlantico.es>
2018-09-06 16:26:54 +0200

    justificante

--- resources/views/resumen_alumnos/table.blade.php
+++ resources/views/resumen_alumnos/table.blade.php
@@ -12,6 +12,7 @@
                     <th>Fecha evento</th>
                     <th>Horas</th>
                     <th>Validado</th>
+                    <th>Justificante</th>
                     <th colspan="3">Acciones
                             @can('resumenAlumnos-create')
                                 <a  class="glyphicon  plus btn-sm glyphicon-plus"  href="{!! route('resumenAlumnos.create') !!}"></a>
@@ -28,6 +29,7 @@
                         <td>{!! $resumenAlumnos->fechaEvento !!}</td>
                         <td>{!! $resumenAlumnos->horas !!}</td>
                         <td>{!! $resumenAlumnos->validado !!}</td>
+                        <td>{!! $resumenAlumnos->justificante !!}</td>
                         <td>
                             {!! Form::open(['route' => ['resumenAlumnos.destroy', $resumenAlumnos->id], 'method' => 'delete']) !!}
                             <div class='btn-group'>
